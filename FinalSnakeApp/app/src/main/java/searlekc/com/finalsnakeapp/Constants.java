eeb8a51acf126a970510c7051d2795f929c3039c
AD\searlekc <searlekc@msoe.edu>
2018-11-13 07:49:46 -0600

    Deleted useless class

--- FinalSnakeApp/app/src/main/java/searlekc/com/finalsnakeapp/Constants.java
+++
@@ -1,7 +0,0 @@
-package searlekc.com.finalsnakeapp;
-
-public class Constants {
-    private static final String INSTANCE_ADDRESS = "searlekcsnakefinal.us1a.cloud.realm.io";
-    static final String AUTH_URL = "https://" + INSTANCE_ADDRESS + "/auth";
-    static final String REALM_BASE_URL = "realms://" + INSTANCE_ADDRESS;
-}
